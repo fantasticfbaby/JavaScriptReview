d37585aef9591c96e0c0fbe0d7817e61b027552f
fantasticfbaby <1092902073@qq.com>
2019-03-25 00:08:25 +0800

    yuanxingmoshi

--- test.js
+++
@@ -1,8 +0,0 @@
-let obj1 = new Object()
-obj1.a = 1
-let obj2 = new Object()
-obj2.a = 2
-let arr = [obj1, obj2]
-arr.forEach(function (item) {
-    console.log(this.toString())
-})
---
+++ 对象/createObject.js
@@ -0,0 +1,43 @@
+//工厂模式
+function createPerson(name) {
+    var o = new Object();
+    o.name = name;
+    o.sayname = function () {
+        console.log(this.name)
+    }
+    return o;
+}
+var person = createPerson("Bob");
+person.sayname();
+
+//构造函数模式
+function Person(name) {
+    this.name = name;
+    this.sayname = function () {
+        console.log(this.name)
+    }
+}
+person = new Person("Lili");
+person.sayname();
+
+Person("Alice"); //构造函数当普通函数使用默认会绑定到global
+global.sayname() // 浏览器中全局对象是window
+
+var o = new Object()
+Person.call(o, "David");
+o.sayname()
+
+var args = ["EEE", 11, 'male']
+function Person1(name, age, sex) {
+    this.name=name;
+    this.age = age;
+    this.sex = sex;
+    this.sayage = function () {
+        console.log(this.sex)
+    }
+}
+Person1.apply(o, args)
+console.log(o) // 发现sayname方法依然存在, 可以用于JavaScript的继承
+o.sayage()
+
+// 原型模式
